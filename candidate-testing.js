13176da1c0a1597ab72d97b1fbf70a9e73f6bfc4
Lisa Rice <lisarivas314@gmail.com>
2021-02-20 16:26:15 +0000

    YAS GIRL - ASSIGNMENT COMPLETE

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,39 +1,48 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
 let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
-let correctAnswer;
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  'Who was the first American woman in space? ',
+  'True or false: 5000 meters = 5 kilometers. ',
+  '(5 + 3)/2 * 10 = ? ',
+  'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
+  'What is the minimum crew size for the ISS? '
+  ];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
+let successAnswers = 0 
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name? ")
+  console.log (`Hi, ${candidateName}!`)
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  for (let i = 0; i < questions.length; i++) {
+   candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
-function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
-  
-
-  return grade;
-}
+function gradeQuiz(candidateAnswers){
+  console.log(`\nCandidate Name: ${candidateName}`)
+  for (let i = 0; i < questions.length; i++) { 
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+      successAnswers = successAnswers + 1 
+    } 
+    console.log(`${1 + i}) ${questions[i]} \nYour Answer: ${candidateAnswers[i]} \nCorrect Answer: ${correctAnswers[i]}\n`)
+  }
+  let grade = successAnswers/5*100
+  let passFail = 'FAILED';
+  if (grade >= 80){
+    passFail = 'PASSED'
+  }
+  console.log(`>>> Overall Grade: ${grade}% (${successAnswers} of 5 responses correct) <<<\n>>> Status: ${passFail} <<<`)
+};
 
 function runProgram() {
   askForName();
